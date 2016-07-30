d0323e38938ab75bd764e5b75beddf770f7dd684
sriEakambaram <sridevire2013@gmail.com>
2016-07-30 22:24:46 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+class Fibonacci
+{
+public static void main(String args[])
+{
+int n=Integer.ParseInt(args[0]);
+int a;
+int b=0;
+nt c=1;
+for(int i=0;i<num;i++)
+{
+a=b;
+b=c;
+c=a+b;
+}
+System.out.println(c);
+}
+}
