2d922b9bcfea324267fabbd678dfdd8b14101b7a
sriEakambaram <sridevire2013@gmail.com>
2016-07-30 21:19:08 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,15 @@
+class Alphabet
+{
+public static void main(String args[])
+{
+char c;
+if(c>=65 &&c<=90 ||c>=97 &&c<=122)
+{
+System.out.println("Alphabet");
+}
+else
+{
+System.out.println("not Alphabet");
+}
+}
+}
