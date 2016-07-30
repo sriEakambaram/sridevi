a0ff3a9c4cec9f8e2456bc42fc100581e1990166
sriEakambaram <sridevire2013@gmail.com>
2016-07-30 21:48:50 +0530

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Reverse
 {
-public static void main(System.in)
+public static void main(String args[])
 {
 Scanner s=new Scanner(System.in);
 System.out.println("enter the numbers");
